77514474ae97056465539a9378c9af1a03fcd57c
suda-3156 <sudasu0109@gmail.com>
2024-11-26 00:06:36 +0900

    Improve: alias command v

--- zsh/sync/aliases.zsh
+++ zsh/sync/aliases.zsh
@@ -10,7 +10,21 @@ alias e='exit'
 alias py='python3'
 alias pip='pip3'
 
-alias v='nvim .'
+# alias v='nvim .'
+alias v='sh -c "
+  file=\$(ls -a -F | grep -v / | grep -v .DS_Store | sed s/\*$//g | head -n 1);
+  if [ -z \"\$file\" ]; then
+    nvim .;
+  else
+    nvim \$file;
+  fi
+"'
+# ls -a (隠しファイルも表示) -F (ディレクトリなら末尾に/をつけるなど) 
+# grep -v / (-vは一致しないもの→ / を含まないもの)
+# mac os のやつ？はいじりたくないので外す
+# sed s/\*$//g (テキストデータに対して、一行ずつ処理を行う。s=呪文, /A/B/=AをBに置換 g=全部置換 \*は特殊文字？)
+# 一行だけの置換なら、(変数名)/(置換前)/(置換後)
+# head -n 1 (表示する, -nで表示行数を指定)
 
 alias lsa='ls -a'
 
