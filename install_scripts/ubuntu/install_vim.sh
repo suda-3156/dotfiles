05cbff8ce44a8385871fc8f72819229540fdd05c
suda-3156 <sudasu0109@gmail.com>
2025-04-20 19:55:33 +0900

    wip

--- install_scripts/ubuntu/install_vim.sh
+++ install_scripts/ubuntu/install_vim.sh
@@ -1,4 +1,23 @@
-# wip
+#!/usr/bin/env bash
 
-# sudo apt install -y vim
-# sudo update-alternatives --config editor
+set -eu
+
+source "$(dirname "${BASH_SOURCE[0]}")/../utils.sh"
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+CONFIG_DIR="$SCRIPT_DIR/../../.config"
+
+log "INFO" "Starting Vim installation..."
+
+check_install "vim"
+log "INFO" "Vim installed: $(vim --version)"
+
+update-alternatives --set editor /usr/bin/vim.basic
+if [[ $? -eq 0 ]]; then
+    log "INFO" "Successfully set Vim as the default editor."
+else
+    log "ERROR" "Failed to set Vim as the default editor."
+    exit 1
+fi
+
+create_symlink "$CONFIG_DIR/vim" "$HOME/.config/vim"
