9ba69104b2135151cbbe789eb20c32a91793aa16
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2025-06-01 14:11:22 +0900

    update:

--- .config/zsh/sync/aliases.zsh
+++ .config/zsh/sync/aliases.zsh
@@ -10,45 +10,65 @@ ABBR_REGULAR_ABBREVIATION_GLOB_PREFIXES+=(
   '*; '
 )
 
-abbr -S c='clear' >> /dev/null
-abbr -S e='exit' >> /dev/null
-abbr -S ll='ls -l' >> /dev/null
-abbr -S la='ls -la' >> /dev/null
-abbr -S s='source' >> /dev/null
+abbr -S -q c='clear'
+abbr -S -q e='exit'
+abbr -S -q ll='ls -lh'
+abbr -S -q la='ls -lAh'
+abbr -S -q s='source'
+abbr -S -q rd='rmdir'
+abbr -S -q md='mkdir -p %'
+abbr -S -q run-help='man %'
+abbr -S -q which-command='whence %'
+abbr -S -q ..='cd ../'
+abbr -S -q ...='cd ../../'
+abbr -S -q ....='cd ../../../'
 
-abbr -S doco='docker compose' >> /dev/null
-abbr -S doce='docker compose exec' >> /dev/null
-abbr -S docr='docker compose run' >> /dev/null
+abbr -S -q doco='docker compose'
+abbr -S -q doce='docker compose exec'
+abbr -S -q docr='docker compose run'
 
-abbr -S v='nvim' >> /dev/null
-abbr -S co='code .' >> /dev/null
+abbr -S -q v='nvim'
+abbr -S -q co='code .'
 
-abbr -S py='python' >> /dev/null
-abbr -S lg='lazygit' >> /dev/null
-abbr -S h='history 16' >> /dev/null
-abbr -S mkdc='mkdir % && cd $_' >> /dev/null
+abbr -S -q py='python'
+abbr -S -q lg='lazygit'
+abbr -S -q h='history 16'
+abbr -S -q mkdc='mkdir % && cd $_'
 
-abbr -S g='git' >> /dev/null
-abbr -S gst='git status' >> /dev/null
-abbr -S gcm='git commit -m "%' >> /dev/null
-abbr -S gc='git commit' >> /dev/null
-abbr -S ga='git add' >> /dev/null
-abbr -S gaa='git add -A' >> /dev/null
-abbr -S gac='git add -A && git commit -m "%' >> /dev/null
+abbr -S -q g='git'
+abbr -S -q gst='git status'
+abbr -S -q gcm='git commit -m "%'
+abbr -S -q gc='git commit'
+abbr -S -q ga='git add'
+abbr -S -q gaa='git add -A'
+abbr -S -q gac='git add -A && git commit -m "%'
 
-abbr -S t='tmux' >> /dev/null
-abbr -S tt='tmux a -t' >> /dev/null
-abbr -S tn='tmux new -s' >> /dev/null
-abbr -S tl='tmux ls' >> /dev/null
-abbr -S ta='tmux a -t' >> /dev/null
+abbr -S -q t='tmux'
+abbr -S -q tt='tmux a -t'
+abbr -S -q tn='tmux new -s'
+abbr -S -q tl='tmux ls'
+abbr -S -q ta='tmux a -t'
 
-abbr -S ghcr='gh repo create % --private --add-readme -c' >> /dev/null
-abbr -S ghmrr='gh repo create % --private --push -s .' >> /dev/null
+abbr -S -q ghcr='gh repo create % --private --add-readme -c'
+abbr -S -q ghmrr='gh repo create % --private --push -s .'
 
-abbr -S op='open .' >> /dev/null
+abbr -S -q op='open .'
+
+function tp() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: tp <path/to/file>"
+    return 1
+  fi
+
+  local filepath="$1"
+  local dirpath="${filepath:h}"
+
+  mkdir -p "$dirpath" && touch "$filepath"
+}
 
 alias ls='ls --color=auto'
 
 alias mv='mv -i'
 alias rm='rm -i'
 alias cp='cp -ir'
+
