27f79723cfe0f8cb599c11f7862d84571b68947a
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2025-12-24 21:15:08 +0900

    move(karabiner): change karabiner directory and improve setup script

---
+++ karabiner/README.md
@@ -0,0 +1,15 @@
+# Karabiner Elements
+
+[Karabiner Elements](https://karabiner-elements.pqrs.org/)
+
+## Simple modifications
+
+- `right_command` -> `fn`
+- Disable `right_option`
+
+## Setup
+
+1. Install `Karabiner Elements` and set up the simple modifications above.
+2. Execute `setup.sh`.
+   - This will create a symbolic link to `$HOME/.config/karabiner/assets/complex_modifications` referencing `dotfiles/karabiner/complex_modifications`
+3. Open Karabiner Elements and enable them using 'Add predefined rules'.
---
+++ karabiner/setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -eu
+
+PJ_ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+LINK_PATH="$HOME/.config/karabiner/assets"
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
+if [[ -L "$LINK_PATH/complex_modifications" ]]; then
+    echo "INFO: Backing up existing $LINK_PATH/complex_modifications to $LINK_PATH/complex_modifications.bak.$TIMESTAMP"
+    mv "$LINK_PATH/complex_modifications" "$LINK_PATH/complex_modifications.bak.$TIMESTAMP"
+    if [[ ! $? -eq 0 ]]; then
+      echo "ERROR: Failed to move existing link: $LINK_PATH/complex_modifications"
+      exit 1
+    fi
+fi
+
+if [[ ! -d "$LINK_PATH" ]]; then
+    echo "INFO: Creating directory: $LINK_PATH"
+    mkdir -p "$LINK_PATH"
+    if [[ ! $? -eq 0 ]]; then
+      echo "ERROR: Failed to create directory: $LINK_PATH"
+      exit 1
+    fi
+fi
+
+ln -s "$PJ_ROOT_DIR/karabiner/complex_modifications" "$LINK_PATH/complex_modifications"
+
+if [[ $? -eq 0 ]]; then
+    echo "INFO: Successfully created symlink: $PJ_ROOT_DIR/karabiner/complex_modifications -> $LINK_PATH/complex_modifications"
+else
+    echo "ERROR: Failed to create symlink: $PJ_ROOT_DIR/karabiner/complex_modifications -> $LINK_PATH/complex_modifications"
+    exit 1
+fi
