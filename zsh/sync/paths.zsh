5bca98e8e6eea6387d6e26dca99100b7f6e55b31
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2026-02-05 12:44:47 +0900

    update(zsh): consolidate PATH definitions
    - Moves platform-specific PATH definitions from .zshrc files to zsh/sync/paths.zsh.

--- zsh/sync/paths.zsh
+++ zsh/sync/paths.zsh
@@ -1,2 +1,30 @@
 # custom scripts
 export PATH="$PATH:$HOME/dotfiles/bin"
+
+# shellcheck disable=all
+case "$(uname -s)" in
+  Darwin)
+    # go
+    export PATH="$PATH:$(go env GOPATH)/bin"
+
+    # Created by `pipx`
+    export PATH="$PATH:$HOME/.local/bin"
+
+    # GNU utils
+    export PATH="$PATH:$(brew --prefix coreutils)/libexec/gnubin"
+    export PATH="$PATH:$(brew --prefix make)/libexec/gnubin"
+    export PATH="$PATH:$(brew --prefix diffutils)/libexec/gnubin"
+    export PATH="$PATH:$(brew --prefix findutils)/libexec/gnubin"
+    export PATH="$PATH:$(brew --prefix gzip)/libexec/gnubin"
+    export PATH="$PATH:$(brew --prefix gawk)/libexec/gnubin"
+
+    # LLVM
+    export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+    export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
+    export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+    ;;
+  Linux)
+    # nvim with appimage
+    export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+    ;;
+esac
