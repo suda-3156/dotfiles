e40d4e2dfabda89bd588cafebfc3dd677e206cb4
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2026-03-14 16:04:56 +0900

    nvim: improve markdown code block style

--- .config/nvim.symlink/lua/plugins/markdown.lua
+++ .config/nvim.symlink/lua/plugins/markdown.lua
@@ -16,6 +16,41 @@ return {
       right_pad = 4,
       icons = {},
     },
+
+    code = {
+      -- Whether to conceal nodes at the top and bottom of code blocks.
+      conceal_delimiters = false,
+      -- Whether to include the language name above code blocks.
+      language_name = false,
+      -- Whether to include the language info above code blocks.
+      language_info = true,
+      -- Amount of padding to add around the language.
+      -- If a float < 1 is provided it is treated as a percentage of available window space.
+      language_pad = 2,
+      -- Width of the code block background.
+      -- | block | width of the code block  |
+      -- | full  | full width of the window |
+      width = "full",
+      -- Amount of margin to add to the left of code blocks.
+      -- If a float < 1 is provided it is treated as a percentage of available window space.
+      -- Margin available space is computed after accounting for padding.
+      left_margin = 0,
+      -- Amount of padding to add to the left of code blocks.
+      -- If a float < 1 is provided it is treated as a percentage of available window space.
+      left_pad = 2,
+      -- Amount of padding to add to the right of code blocks when width is 'block'.
+      -- If a float < 1 is provided it is treated as a percentage of available window space.
+      right_pad = 2,
+      -- Determines how the top / bottom of code block are rendered.
+      -- | none  | do not render a border                               |
+      -- | thick | use the same highlight as the code body              |
+      -- | thin  | when lines are empty overlay the above & below icons |
+      -- | hide  | conceal lines unless language name or icon is added  |
+      border = "thin",
+      -- Padding to add to the left & right of inline code.
+      inline_pad = 1,
+    },
+
     checkbox = {
       checked = { scope_highlight = "@markup.strikethrough" },
       custom = {
