ba07e33cb985f93ab4a9c76c4ae017e80150e318
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2026-03-15 14:11:51 +0900

    nvim: use default preset for blink.cmp

--- .config/nvim.symlink/lua/plugins/blink-cmp.lua
+++ .config/nvim.symlink/lua/plugins/blink-cmp.lua
@@ -10,34 +10,24 @@ return {
   ---@type blink.cmp.Config
   opts = {
     keymap = {
-      preset = "super-tab",
+      preset = "default",
 
       -- Following commentouted lines are defined by default preset
-      -- I'm using <C-space> as the tmux prefix
-      ["<C-\\>"] = { "show", "show_documentation", "hide_documentation" },
+      -- ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
       -- ["<C-e>"] = { "hide", "fallback" },
-
-      -- ["<Tab>"] = {
-      --   function(cmp)
-      --     if cmp.snippet_active() then
-      --       return cmp.accept()
-      --     else
-      --       return cmp.select_and_accept()
-      --     end
-      --   end,
-      --   "snippet_forward",
-      --   "fallback",
-      -- },
-      -- ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
+      -- ["<C-y>"] = { "select_and_accept", "fallback" },
+      --
       -- ["<Up>"] = { "select_prev", "fallback" },
       -- ["<Down>"] = { "select_next", "fallback" },
       -- ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
       -- ["<C-n>"] = { "select_next", "fallback_to_mappings" },
-
+      --
       -- ["<C-b>"] = { "scroll_documentation_up", "fallback" },
       -- ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
+      --
+      -- ["<Tab>"] = { "snippet_forward", "fallback" },
+      -- ["<S-Tab>"] = { "snippet_backward", "fallback" },
+      --
       -- ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
     },
 
