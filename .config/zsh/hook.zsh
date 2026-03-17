3e344af60b52030ace853072fa612a79a777cde9
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2026-03-17 11:37:29 +0900

    zsh: improve chpwd to show entries in one column when they're not more than 20

--- .config/zsh/hook.zsh
+++ .config/zsh/hook.zsh
@@ -1,37 +1,49 @@
-# From: https://gist.github.com/yonchu/3935922
 chpwd() {
 	ls_abbrev
 }
+
+# From: https://gist.github.com/yonchu/3935922
 ls_abbrev() {
 	if [[ ! -r $PWD ]]; then
 		return
 	fi
-	# -a : Do not ignore entries starting with ..
+
+	# -A : Show all entries except for '.' and '..'.
 	# -C : Force multi-column output.
 	# -F : Append indicator (one of */=>@|) to entries.
 	local cmd_ls='ls'
 	local -a opt_ls
-	opt_ls=('-aCF' '--color=always')
+	opt_ls=('-AF' '--color=always')
+
 	case "${OSTYPE}" in
 	freebsd* | darwin*)
-		if type gls >/dev/null 2>&1; then
+		if command -v gls >/dev/null 2>&1; then
 			cmd_ls='gls'
 		else
 			# -G : Enable colorized output.
-			opt_ls=('-aCFG')
+			opt_ls=('-aFG')
 		fi
 		;;
 	esac
-	local ls_result
-	ls_result=$(CLICOLOR_FORCE=1 COLUMNS=$COLUMNS command $cmd_ls ${opt_ls[@]} | sed $'/^\e\[[0-9;]*m$/d')
+
+  local num="$(command ls -1 -A | wc -l | tr -d ' ')"
+  local show_all=false
+
+  if [ "$num" -gt 20 ]; then
+    opt_ls+=('-C')
+    show_all=true
+  fi
+
+  # Remove lines only with color codes by sed
+	local ls_result=$(CLICOLOR_FORCE=1 COLUMNS=$COLUMNS command $cmd_ls ${opt_ls[@]} | sed $'/^\e\[[0-9;]*m$/d')
 
 	local ls_lines=$(echo "$ls_result" | wc -l | tr -d ' ')
 
-	if [ $ls_lines -gt 10 ]; then
+	if [ $ls_lines -gt 10 -a ! show_all ]; then
 		echo "$ls_result" | head -n 5
 		echo '...'
 		echo "$ls_result" | tail -n 5
-		echo "$(command ls -1 -A | wc -l | tr -d ' ') files exist"
+		echo "$(command ls -1 -A | wc -l | tr -d ' ') files"
 	else
 		echo "$ls_result"
 	fi
