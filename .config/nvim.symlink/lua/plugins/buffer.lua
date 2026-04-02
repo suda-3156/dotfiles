f215c308e3f9fa130283ff824c0ba2fb4d051e95
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2026-04-02 17:07:26 +0900

    nvim: update keymap '<leader>D' to clear all buffers except visible to maitain current split views

--- .config/nvim.symlink/lua/plugins/buffer.lua
+++ .config/nvim.symlink/lua/plugins/buffer.lua
@@ -9,19 +9,57 @@ return {
   init = function()
     vim.g.barbar_auto_setup = false
   end,
+  -- spellchecker:off
+  -- stylua: ignore start
   keys = {
-    { "<leader>C",   "<Cmd>BufferCloseAllButCurrent<CR>", desc = "[C]ose [A]ll [B]uffers except for Current" },
-    { "<leader>cbr", "<Cmd>BufferCloseBuffersRight<CR>",  desc = "[C]ose [B]uffers to the [R]ight" },
-    { "<leader>cbl", "<Cmd>BufferCloseBuffersLeft<CR>",   desc = "[C]ose [B]uffers to the [L]eft" },
+    { "<leader>d",  "<cmd>BufferClose<cr>",               desc = "Close Buffer" },
     { "<S-h>",       "<cmd>BufferPrevious<cr>",           desc = "Prev Buffer" },
     { "<S-l>",       "<cmd>BufferNext<cr>",               desc = "Next Buffer" },
-    { "<leader>dd",  "<cmd>BufferClose<cr>",              desc = "Close Buffer" },
+    { "<leader>cbr", "<Cmd>BufferCloseBuffersRight<CR>",  desc = "[C]ose [B]uffers to the [R]ight" },
+    { "<leader>cbl", "<Cmd>BufferCloseBuffersLeft<CR>",   desc = "[C]ose [B]uffers to the [L]eft" },
   },
+  -- stylua: ignore end
+  -- spellchecker:on
 
   config = function()
     require("barbar").setup({
       auto_hide = 0,
     })
+
+    -- Close all buffers except those visible in windows
+    local function close_all_but_visible()
+      -- Get all buffers currently visible in windows
+      local visible_buffers = {}
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        visible_buffers[buf] = true
+      end
+
+      -- Close all other buffers
+      for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+        if visible_buffers[bufnr] then
+          goto continue
+        end
+
+        -- Only close if it's a normal buffer (not special buffers)
+        local buftype = vim.api.nvim_get_option_value("buftype", { buf = bufnr })
+        if buftype ~= "" then
+          goto continue
+        end
+
+        -- and it doesn't have any unsaved changes
+        local is_modified = vim.api.nvim_get_option_value("modified", { buf = bufnr })
+        if is_modified then
+          goto continue
+        end
+
+        pcall(vim.api.nvim_buf_delete, bufnr, { force = false })
+        ::continue::
+      end
+    end
+    vim.keymap.set("n", "<leader>D", function()
+      close_all_but_visible()
+    end, { desc = "Close All Buffers except Visible" })
 
     -- transparent tab line
     local function fix_tabfill()
