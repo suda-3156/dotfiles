dd950de514abe129b3b33393097c825dc5568f79
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2026-04-05 11:33:58 +0900

    nvim: add an abbr for replacement

--- .config/nvim.symlink/lua/config/abbr.lua
+++ .config/nvim.symlink/lua/config/abbr.lua
@@ -1,16 +1,30 @@
 -- From: https://zenn.dev/kawarimidoll/books/6064bf6f193b51/viewer/e5428b
 -- abbreviation only for ex-command
-local function abbrev_excmd(lhs, rhs, opts)
+local function abbr(lhs, rhs, opts)
   vim.keymap.set("ca", lhs, function()
-    return vim.fn.getcmdtype() == ":" and rhs or lhs
+    if vim.fn.getcmdtype() ~= ":" or not rhs or rhs == "" then
+      return lhs
+    end
+
+    if type(rhs) == "function" then
+      return rhs()
+    else
+      return rhs
+    end
   end, vim.tbl_extend("force", { expr = true }, opts))
 end
 
-abbrev_excmd("qw", "wq", { desc = "fix typo" })
-abbrev_excmd("lup", "lua =", { desc = "lua print" })
+abbr("qw", "wq", { desc = "Fix typo" })
+abbr("lup", "lua =", { desc = "Lua print" })
 
 vim.api.nvim_create_user_command("Restart", function()
   vim.cmd("AutoSession save")
   vim.cmd.restart()
 end, { desc = "Restart current Neovim session" })
-abbrev_excmd("rst", "Restart", { desc = "save session and restart" })
+abbr("rst", "Restart", { desc = "Save session and restart" })
+
+-- From: https://zenn.dev/vim_jp/articles/2023-06-30-vim-substitute-tips
+abbr("s", function()
+  vim.fn.getchar()
+  return "%s///g<Left><Left>"
+end, { desc = "Replace all matched in current buffer" })
