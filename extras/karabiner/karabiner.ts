e9113866d85d04f770d6abbfa608163fe0579464
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2026-04-07 23:32:52 +0900

    karabiner: WIP use karabiner.ts

---
+++ extras/karabiner/karabiner.ts
@@ -0,0 +1,82 @@
+// From: https://github.com/kawarimidoll/dotfiles/blob/master/karabiner/karabiner.ts
+import * as k from "karabiner_ts";
+import { volumeBrightnessRules } from "./rules/volume-brightness.ts";
+import { fnNumbersRules } from "./rules/fn-numbers.ts";
+import { hyperNumbersRules } from "./rules/hyper-numbers.ts";
+import { capsTabHyperRules } from "./rules/caps-tab-hyper.ts";
+import { type ExtendedProfile } from "./type.ts";
+import {
+  APPLE_INTERNAL_KEYBOARD,
+  applySimpleModifications,
+  KEYCHRON_K7_PRO,
+  rightCmdToFn,
+} from "./simple-modifications.ts";
+
+const PROFILE_NAME = "Default profile";
+
+// Write complex modifications
+k.writeToProfile(PROFILE_NAME, [
+  ...volumeBrightnessRules,
+  ...fnNumbersRules,
+  ...hyperNumbersRules,
+  ...capsTabHyperRules,
+]);
+
+// Write global settings
+k.writeToGlobal({
+  show_in_menu_bar: true,
+});
+
+// Read and update config with simple modifications and some parameters
+const configPath = [
+  Deno.env.get("HOME")!,
+  ".config",
+  "karabiner",
+  "karabiner.json",
+].join("/");
+
+const config = JSON.parse(
+  Deno.readTextFileSync(configPath),
+) as k.KarabinerConfig;
+
+const profile = config?.profiles.find(
+  (v) => v.name === PROFILE_NAME,
+)! as ExtendedProfile;
+
+if (!profile) {
+  console.error(`Profile ${PROFILE_NAME} not found`);
+  Deno.exit(1);
+}
+
+// Set profile parameters
+profile.parameters = {
+  delay_milliseconds_before_open_device: 1000,
+};
+
+// Set complex modifications parameters
+profile.complex_modifications.parameters = {
+  "basic.simultaneous_threshold_milliseconds": 50,
+  "basic.to_delayed_action_delay_milliseconds": 500,
+  "basic.to_if_alone_timeout_milliseconds": 200,
+  "basic.to_if_held_down_threshold_milliseconds": 200,
+};
+
+// Set virtual HID keyboard settings
+profile.virtual_hid_keyboard = {
+  country_code: 0,
+  indicate_sticky_modifier_keys_state: true,
+  keyboard_type_v2: "ansi",
+};
+
+// Apply simple modifications to devices
+applySimpleModifications(profile, APPLE_INTERNAL_KEYBOARD, rightCmdToFn);
+applySimpleModifications(profile, KEYCHRON_K7_PRO, rightCmdToFn);
+
+// Write back to file
+const json = JSON.stringify(config, null, 2);
+Deno.writeTextFile(configPath, json).catch((err) => {
+  console.error(err);
+  Deno.exit(1);
+});
+
+console.log(`✓ Profile ${PROFILE_NAME} updated with all settings.`);
---
+++ extras/karabiner/simple-modifications.ts
@@ -0,0 +1,80 @@
+// From: https://github.com/kawarimidoll/dotfiles/blob/6e1a495bd066e5be079933e1c896ae3416366e30/karabiner/karabiner.ts#L8-L50
+import * as k from "karabiner_ts";
+import { ExtendedProfile, SimpleModification } from "./type.ts";
+
+// Helper function to create simple modifications
+export function simpleModifications(
+  params: {
+    from: Parameters<typeof k.getKeyWithAlias>[0];
+    to:
+      | Parameters<typeof k.getKeyWithAlias>[0]
+      | { apple_vendor_top_case_key_code: string };
+  }[],
+): SimpleModification[] {
+  return params.map((p) => {
+    const from = k.getKeyWithAlias(p.from);
+    if (
+      typeof p.to === "object" &&
+      "apple_vendor_top_case_key_code" in p.to
+    ) {
+      return {
+        from: { key_code: from },
+        to: [p.to],
+      };
+    }
+    const to = k.getKeyWithAlias(
+      p.to as Parameters<typeof k.getKeyWithAlias>[0],
+    );
+    return { from: { key_code: from }, to: [{ key_code: to }] };
+  });
+}
+
+// Helper function to apply simple modifications to a device
+export function applySimpleModifications(
+  profile: ExtendedProfile,
+  identifiers: k.DeviceIdentifier,
+  simple_modifications: SimpleModification[],
+) {
+  const targetDevice = profile.devices?.find(
+    (d) =>
+      d.identifiers.is_keyboard === identifiers.is_keyboard &&
+      (identifiers.vendor_id === undefined ||
+        d.identifiers.vendor_id === identifiers.vendor_id) &&
+      (identifiers.product_id === undefined ||
+        d.identifiers.product_id === identifiers.product_id),
+  );
+
+  if (targetDevice) {
+    targetDevice.simple_modifications = simple_modifications;
+  } else {
+    profile.devices = [
+      ...(profile.devices || []),
+      { identifiers, simple_modifications },
+    ];
+  }
+}
+
+// Device identifiers
+export const APPLE_INTERNAL_KEYBOARD = {
+  is_keyboard: true,
+} as const satisfies k.DeviceIdentifier;
+
+export const KEYCHRON_K7_PRO = {
+  product_id: 624,
+  vendor_id: 13364,
+  is_keyboard: true,
+} as const satisfies k.DeviceIdentifier;
+
+// Simple modification definitions
+// Replace keys right next to the space bar to fn keys
+export const rightCmdToFn = simpleModifications([
+  { from: "r⌘", to: { apple_vendor_top_case_key_code: "keyboard_fn" } },
+]);
+
+export const rightCtrlToFn = simpleModifications([
+  { from: "r⌃", to: { apple_vendor_top_case_key_code: "keyboard_fn" } },
+]);
+
+export const rightOptToFn = simpleModifications([
+  { from: "r⌥", to: { apple_vendor_top_case_key_code: "keyboard_fn" } },
+]);
