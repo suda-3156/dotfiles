8b5429e3623ed90e37c28c3b74a61c50526ebf0a
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2026-04-09 23:16:13 +0900

    nvim: setup basedpyright

---
+++ .config/nvim.symlink/after/lsp/basedpyright.lua
@@ -0,0 +1,12 @@
+-- Ref: https://docs.basedpyright.com/v1.21.0/configuration/language-server-settings
+return {
+  settings = {
+    basedpyright = {
+      analysis = {
+        typeCheckingMode = "standard",
+        diagnosticMode = "workspace",
+        autoImportCompletions = true,
+      },
+    },
+  },
+}
