6764b65570620f56f1ba313e2925564b02c869b1
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2026-04-24 17:22:35 +0900

    karabiner: change keys to switch imkit to japanese

--- extras/karabiner/rules/imkit.ts
+++ extras/karabiner/rules/imkit.ts
@@ -9,11 +9,17 @@ export const imkitRules = [
       .toIfAlone({ key_code: "japanese_eisuu" })
       .description(`Tap left_command alone to switch to japanese_eisuu`)
       .parameters({ "basic.to_if_held_down_threshold_milliseconds": 100 }),
+    // k
+    //   .map({ key_code: "right_command", modifiers: { optional: ["any"] } })
+    //   .to({ key_code: "right_command", lazy: true })
+    //   .toIfAlone({ key_code: "japanese_kana" })
+    //   .description(`Tap right_command alone to switch to japanese_kana`)
+    //   .parameters({ "basic.to_if_held_down_threshold_milliseconds": 100 }),
     k
-      .map({ key_code: "right_command", modifiers: { optional: ["any"] } })
-      .to({ key_code: "right_command", lazy: true })
-      .toIfAlone({ key_code: "japanese_kana" })
-      .description(`Tap right_command alone to switch to japanese_kana`)
-      .parameters({ "basic.to_if_held_down_threshold_milliseconds": 100 }),
+      .map({
+        key_code: "semicolon",
+        modifiers: { mandatory: ["right_command"] },
+      })
+      .to({ key_code: "japanese_kana" }),
   ]),
 ];
